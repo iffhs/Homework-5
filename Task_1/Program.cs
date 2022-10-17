8a9d618c54de10c1c8043d7077754f68c86f45e9
Egor <kosterev2k17@yandex.ru>
2022-10-17 21:06:58 +0300

    first commit

---
+++ Task_1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArray(int len, int minLimit, int maxLimit)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minLimit, maxLimit + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int Count(int[] array)
+{
+    int a = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            a++;
+        }
+    }
+    return a;
+}   
+
+int[] array = CreateArray(4, 100, 999);
+PrintArray(array);
+Console.WriteLine($"Выводит числа диапозона: {Count(array)}");
---
+++ Task_3/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] CreateArray(int len, int minLimit, int maxLimit)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minLimit, maxLimit + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int Difference(int[] array)
+{
+    int a = int.MaxValue;
+    int b = int.MinValue;
+    foreach (int n in array)
+    {
+        if(n > b)
+        {
+            b = n;
+        }
+        if (n < a)
+        {
+            a = n;
+        }
+    }
+    return b - a;
+}
+
+int[] array = CreateArray(6, 1, 20);
+PrintArray(array);
+Console.WriteLine($"Выводит разницу: {Difference(array)}");
